d8900321ae2b83da891aa6aeded91b7d6b734633
Gabogg07 <gabrieleduardogg@gmail.com>
2017-01-25 21:14:34 -0400

    Proyecto Terminado
    proyecto comentado

--- FirstModule.py
+++ FirstModule.py
@@ -1,7 +1,8 @@
 '''
 Created on Jan 23, 2017
 
-@author: gabriel
+@author: Fabiana Acosta 10-10005
+         Gabriel Gutierrez 13-10625
 '''
 import datetime
 from calendar import monthrange
@@ -29,37 +30,32 @@ Milss=input()
 global terminado,Restos
 terminado=False
 Restos=1
-Anoe=2017
-Mese=12
-Diae=31
 
 ######################################################################################
-
-def horasPorDia(I,F,C):
+#FUNCION horasPorDia
+# funcion encargada de calcular para un dia, cuantas horas de servicio son. Toma en cuenta si es dia de semana.
+#ENTRADA:
+#    - I : objeto datetime.datetime , contiene la hora inicial desde la cual se van a calcular las horas
+#    - F : objeto datetime.datetime , contiene la hora hasta la cual se van a calcular el numero de horas
+#
+#SALIDA:
+#    - hours: tupla que contiene el numero de horas de servicio, en su primera posicion las de dia de semana
+#             y en la segunda las de fines de semana.
+#    - inicio: objeto datetime.datetime , contiene la fecha de inicio del dia siguiente a evaluar
+#######################################################################################
+def horasPorDia(I,F):
     global Restos
-    print("------------------------------------------------")
-    print(I, "  HASTA ", F)
-    print("------------------------------------------------")
     hours=[0,0]
-    if(I.weekday()<5 ):
-        print("Es un dia de semana ")
-    else:
-        print("Es fin de semana")
     delta=F-I
-    print("DELTA", delta)
+
     horas=delta.seconds//3600
     resto=delta.seconds%3600
     Restos=Restos+resto
     
-    print("resto= ",resto)
     
     if(resto!=0):
+        #Como hubo segundos de sobra en la hora, se agrega la hora extra
         horas+=1
-        print("son horas no exactas, el resto es: ",resto)
-    else:
-        print("Son horas exactas")
-    
-    print(horas," Numero de horas")
 
     if(I.weekday()<5):
         hours[0]=horas
@@ -68,22 +64,31 @@ def horasPorDia(I,F,C):
     
     #CHEQUEO SI EL DIA SIGUIENTE A TRABAJAR ESTA EN OTRO MES O EN ESE MISMO.
     if(I.date().day+1 in range(1,monthrange(I.date().year, I.date().month)[1]+1)):
-        print("NO CAMBIO DE MES")
+        #No hubo cambio de mes
         inicio=datetime.datetime(I.date().year,I.date().month,I.date().day+1,0,0,0,0)
     else:
-        print("CAMBIO DE MES")
+        #Hubo cambio de mes
         if(I.date().month==12):
+            #Hubo cambio de anio
             inicio=datetime.datetime(I.date().year+1,1,1,0,0,0,0)
-            print("CAMBIO DE anio")
         else:
+            #No hubo cambio de anio
             inicio=datetime.datetime(I.date().year,I.date().month+1,1,0,0,0,0)
-            print("NO CAMBIO DE anio")
-    #inicio=datetime.datetime(I.year(),I.month(),I.days(),0,0,0,0)
-    print(inicio ,"ESTO ES EL INCIO TEMP")
     return hours,inicio
 
-######################################################################3
+####################################################################################
 
+####################################################################################
+# Funcion calcularPrecio
+#    funcion que dada una tarifa y unas horas de servicio, calcula el monto a pagar
+# ENTRADA:
+#    - tupla Tarifa, tupla que tiene en la primera posicion la tarifa para dia de semana, y en la segunda
+#                    para los fines de semana
+#    - tupla tiempoDeServicio, tupla de datetime.datetime que contiene las fechas y horas de entrada y salida para
+#                              calcular el servicio.
+# SALIDA:
+#    - decimal monto, variable que contiene el monto a pagar por las horas de servicio
+####################################################################################
 def calcularPrecio(Tarifa,tiempoDeServicio):
     global terminado,Restos
     InicioDeServicio=tiempoDeServicio[0]
@@ -95,49 +100,43 @@ def calcularPrecio(Tarifa,tiempoDeServicio):
     while not terminado:
         acum=[0,0]
         if(inic.date()==FinDeServicio.date()):
-            #ES EL MISMO DIA
-                acum,inic=horasPorDia(inic, FinDeServicio, Tarifa)
+                acum,inic=horasPorDia(inic, FinDeServicio)
                 terminado=True
                 
         else:
                 temp=datetime.datetime(inic.date().year,inic.date().month,inic.date().day,23,59,59,999)
-                acum,inic=horasPorDia(inic, temp, Tarifa)
-                print(inic, "INICIO LUEGO DE LA PRIMERA CORRIDA")
+                acum,inic=horasPorDia(inic, temp)
                 
         totalh[0]+=acum[0]
         totalh[1]+=acum[1]
-        print(totalh)
-        print("-------------CORRIDA----------------")
-    #print(Restos)
-    #print(totalh)
     if(Restos>=3599):
         if(FinDeServicio.weekday()<5):
             totalh[0]=totalh[0]-1
         else:
             totalh[1]=totalh[1]-1
-    #print(totalh)
     monto=totalh[0]*Tarifa[0]+ totalh[1]*Tarifa[1]
     return monto
 
 
 ######################################################################
 
-#WEEKDAY 5 Y 6 SON SABADO Y DOMINGO
-InicioDeServicio=datetime.datetime(Anoe,Mese,Diae,1,10,0,0)
-FinDeServicio=datetime.datetime(Anoe+1,Mese-11,Diae-30,1,10,0,0)
+#DECLARACION DE VARIABLES CABLEADAS
+Anoe=2017
+Mese=5
+Diae=17
+InicioDeServicio=datetime.datetime(Anoe,Mese,Diae,8,0,0,0)
+FinDeServicio=datetime.datetime(Anoe,Mese,Diae+6,21,0,0,0)
 
+# TarifaSem y TarifaFin son dos tuplas, en su primera posicion tiene los bolivares y en la segunda los centimos
 TarifaSem=[2,5]
 vSem=TarifaSem[0]+(TarifaSem[1]/100)
 TarifaFin=[5,0]
 vFin=TarifaFin[0]+(TarifaFin[1]/100)
 
-tiempoDeServicio=[InicioDeServicio,FinDeServicio]
-
-print(vSem,"  ",vFin)
-        
+#Cuerpo del codigo
+tiempoDeServicio=[InicioDeServicio,FinDeServicio]       
 Tarifa=[vSem,vFin]
-print((FinDeServicio-InicioDeServicio).seconds)
-print((FinDeServicio-InicioDeServicio).days)
+print((FinDeServicio-InicioDeServicio))
 
 if((FinDeServicio-InicioDeServicio).seconds<900 and (FinDeServicio-InicioDeServicio).days==0):
     print("El tiempo de servicio debe ser al menos 15 minutos")
@@ -146,40 +145,4 @@ elif((FinDeServicio-InicioDeServicio).days>7):
 else:
     pago=calcularPrecio(Tarifa, tiempoDeServicio)
     bolivares,centimos=divmod(pago, 1)
-    print("Se deben: ",bolivares," bolivares con: ",centimos, "centimos")
-#tiempoDeTrabajo=[InicioDeServicio,FinDeServicio]
-#print(InicioDeServicio, "   " ,FinDeServicio)
-#print((FinDeServicio - InicioDeServicio))
-'''
-inic=InicioDeServicio
-fin=FinDeServicio
-
-totalh=[0,0]
-print(InicioDeServicio.date()==FinDeServicio.date())
-
-while not terminado:
-    acum=[0,0]
-    if(inic.date()==FinDeServicio.date()):
-        #ES EL MISMO DIA
-            acum,inic=horasPorDia(inic, FinDeServicio, Tarifa)
-            terminado=True
-            
-    else:
-            temp=datetime.datetime(inic.date().year,inic.date().month,inic.date().day,23,59,59,999)
-            acum,inic=horasPorDia(inic, temp, Tarifa)
-            print(inic, "INICIO LUEGO DE LA PRIMERA CORRIDA")
-    totalh[0]+=acum[0]
-    totalh[1]+=acum[1]
-    print(totalh)
-    print("-------------CORRIDA----------------")
-    
-if(Restos>=3599):
-    if(FinDeServicio.weekday()<5):
-        totalh[0]=totalh[0]-1
-    else:
-        totalh[1]=totalh[1]-1
-
-print(totalh)
-print(totalh[0]+totalh[1])
-'''
-
+    print("Se deben: ",bolivares," bolivares con: ",centimos*100, "centimos")
